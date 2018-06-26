62f5d4b9de00f7b08649657f565e7391db32d3bf
GarRuru <nick92251@gmail.com>
2018-06-26 22:39:55 +0800

    Initial Commit

---
+++ MovieFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieFun
+//
+//  Created by Yu-Hsuan Chen on 2018/6/26.
+//  Copyright © 2018年 Tsung-Han Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
